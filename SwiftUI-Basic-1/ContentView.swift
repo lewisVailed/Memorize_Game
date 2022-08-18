c3b1ccd8332738a0e535317a8b8c5902ac302f6f
Ayberk Bilgiç <lewidvailed@outlook.com>
2022-08-18 03:21:55 +0300

    Initial Commit

---
+++ SwiftUI-Basic-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Basic-1
+//
+//  Created by Ayberk Bilgiç on 18.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Basic-1/SwiftUI_Basic_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Basic_1App.swift
+//  SwiftUI-Basic-1
+//
+//  Created by Ayberk Bilgiç on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Basic_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
