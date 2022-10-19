0079c24a075770cdea99d9d4838a194a2660892a
Ayberk Bilgiç <lewidvailed@outlook.com>
2022-10-19 15:16:44 +0300

    update

--- SwiftUI-Basic-1/extension.swift
+++ SwiftUI-Basic-1/extension.swift
@@ -25,11 +25,33 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
     
     var body: some View {
         let width: CGFloat = 100
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: width))]) {
+        LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
             ForEach(items) { item in
                 content(item).aspectRatio(aspectRatio, contentMode: .fit)
 
             }
         }
+    }
+    
+    private func adaptiveGridItem(width: CGFloat) -> GridItem {
+        GridItem(.adaptive(minimum: width))
+    }
+    
+    private func widthThatFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
+        var columnCount = 1
+        var rowCount = itemCount
+        repeat {
+            let itemWidth = size.width / CGFloat(columnCount)
+            let itemHeight = itemWidth / itemAspectRatio
+            if CGFloat(rowCount) * itemHeight < size.height {
+                break
+            }
+            columnCount += 1
+            rowCount = (itemCount + (columnCount - 1)) / columnCount
+        } while columnCount < itemCount
+        if columnCount > itemCount {
+            columnCount = itemCount
+        }
+        return floor(size.width / CGFloat(columnCount))
     }
 }
